047a17c2411aaf6aeabdd8e280c9824d4ccdd270
Aneesh Pavan Prodduturu <aneeshpavan@gmail.com>
2023-01-29 11:50:27 -0600

    Update

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
     $('.carousel').carousel({
         interval: 3000
         });
-            
     $('.carousel').carousel('cycle');
 });
 
@@ -13,6 +12,7 @@ if (localStorage.lightMode == "dark") {
 
 function toggle_light_mode() {
 var app = document.getElementsByTagName("BODY")[0];
+var rightimage = document.getElementById("rightimg");
 if (localStorage.lightMode == "light") {
     localStorage.lightMode = "dark";
     app.setAttribute("light-mode", "dark");
